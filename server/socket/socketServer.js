bdbc16935d0a7ea738d7ae7726cb2df50f85788f
Sumit <sumitjadhav6262@gmail.com>
2026-03-12 12:33:50 +0530

    phase-7-online-user-system complete

--- server/socket/socketServer.js
+++ server/socket/socketServer.js
@@ -20,9 +20,9 @@ const removeUser = (socketId) => {
 onlineUsers = onlineUsers.filter(user => user.socketId !== socketId);
 };
 
-const getUser = (userId) => {
-return onlineUsers.find(user => user.userId === userId);
-};
+// const getUser = (userId) => {
+// return onlineUsers.find(user => user.userId === userId);
+// };
 
 io.on("connection",(socket)=>{
 
